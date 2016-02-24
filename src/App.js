ed0803cbac37c1c85a483eb4c3410d52f9d8f662
Collin <candrew022@gmail.com>
2016-02-23 20:09:27 -0500

    Created the todo App
    ## Components
    - App
    - CommentForm
    - CommentList
    - Comment
    
    ## Functions
    - HandleSubmit

---
+++ src/App.js
@@ -0,0 +1,115 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+export class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      comments: [
+        {
+          name: 'Pete Hunt',
+          message: 'this is one comment'
+        },
+        {
+          name: 'Jordan Walke',
+          message: 'this is another comment'
+        }
+      ]
+    }
+    this.handleCommentSubmit = this.handleCommentSubmit.bind(this);
+  }
+
+  handleCommentSubmit(comment) {
+    let {comments} = this.state;
+    const newComments = comments.concat([comment]);
+    this.setState({
+      comments: newComments
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Comments</h1>
+        <CommentForm onCommentSubmit={this.handleCommentSubmit}/>
+        <CommentList comments={this.state.comments}/>
+      </div>
+    );
+  }
+}
+
+class CommentForm extends Component {
+
+  constructor(props) {
+    super();
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    let name = ReactDOM.findDOMNode(this.refs.name).value.trim();
+    let message = ReactDOM.findDOMNode(this.refs.message).value.trim();
+
+    if(!message || !name) {
+      return;
+    }
+
+    this.props.onCommentSubmit({name: name, message: message});
+    ReactDOM.findDOMNode(this.refs.name).value = '';
+    ReactDOM.findDOMNode(this.refs.message).value = '';
+    return;
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div>
+          <input type="text" placeholder="Your name" ref="name" />
+        </div>
+        <div>
+          <input type="text" placeholder="Say something" ref="message" />
+        </div>
+        <div>
+          <input type="submit" value="Post" />
+        </div>
+
+      </form>
+    )
+  }
+}
+
+class CommentList extends Component {
+
+  render() {
+
+    const {comments} = this.props;
+
+    let commentNodes = comments.map((comment, i) => {
+      return(
+        <Comment name={comment.name} key={i}>
+          {comment.message}
+        </Comment>
+      )
+    });
+
+    return (
+      <div>
+        {commentNodes}
+      </div>
+    )
+  }
+}
+
+class Comment extends Component {
+  render() {
+    return(
+      <div>
+        <h3>{this.props.name}</h3>
+        <p>
+          {this.props.children}
+        </p>
+      </div>
+    )
+  }
+}
