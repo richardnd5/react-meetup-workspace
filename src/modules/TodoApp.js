008072af8b403d7d741ca73d1f3c219ce320f27c
richardnd5 <nathanrichard@Nathans-MacBook-Pro.local>
2016-03-16 19:48:10 -0700

    Undid the Stateless Component

--- src/modules/TodoApp.js
+++ src/modules/TodoApp.js
@@ -74,30 +74,19 @@ class TodoForm extends Component {
 
 // Do the toggle thingy (down below). Turn these babies to stateless.
 
-const TodoList = ({ todos }) => (
-  <div>
-  {todos.map((todo, i) => {
+class TodoList extends Component {
+  render() {
     return (
-      <Todo text={todo.text} key={i} />
+      <div>
+        {this.props.todos.map( (todo, i) => {
+          return (
+            <Todo text={todo.text} key={i} />
+          )
+        })}
+      </div>
     )
-  })}
-  </div>
-
-)
-
-// class TodoList extends Component {
-//   render() {
-//     return (
-//       <div>
-//       {this.props.todos.map( (todo, i) => {
-//         return (
-//           <Todo text={todo.text} key={i} />
-//         )
-//       })}
-//       </div>
-//     )
-//   }
-// }
+  }
+}
 
 class Todo extends Component {
 
