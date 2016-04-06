0e3ed7d7335a8b015718debb4146520a6a65741f
Zeb Pykosz <zeb.apy@gmail.com>
2016-04-06 13:40:17 -0400

    begin adding fetch requests for comment app

--- src/modules/comments/CommentApp.js
+++ src/modules/comments/CommentApp.js
@@ -7,27 +7,42 @@ export default class CommentApp extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {comments: [
-      {
-        name: 'Pete Hunt',
-        message: 'this is one comment'
-      },
-      {
-        name: 'Jordan Walke',
-        message: 'this is another comment'
-      }
-    ]}
+
+    this.state = {
+      isLoading: false,
+      comments: []
+    }
+
     this.handleCommentSubmit = this.handleCommentSubmit.bind(this);
+    this.fetchComments = this.fetchComments.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({
+      isLoading: true
+    })
+    // mock slower API request to show loading state
+    setTimeout(this.fetchComments, 2000);
+  }
+
+  fetchComments() {
+    fetch('http://localhost:3001/comments')
+      .then((res) => res.json())
+      .then((comments) => {
+        this.setState({
+          isLoading: false,
+          comments
+        });
+      });
   }
 
   handleCommentSubmit(comment) {
-
-    const { comments } = this.state;
-
-    const newComments = comments.concat([comment]);
-
-    this.setState({
-      comments: newComments
+    fetch('http://localhost:3001/comments', {
+      method: 'POST',
+      body: comment
+    }).then((res) => res.json())
+    .then((res) => {
+      console.log(res);
     })
   }
 
@@ -36,7 +51,9 @@ export default class CommentApp extends Component {
       <div>
         <h1>Comments</h1>
         <CommentForm onCommentSubmit={this.handleCommentSubmit}/>
-        <CommentList comments={this.state.comments}/>
+        {this.state.isLoading ? 'Loading...' :
+          <CommentList comments={this.state.comments}/>
+        }
       </div>
     );
   }
