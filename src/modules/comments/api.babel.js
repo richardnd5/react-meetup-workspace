d45471df41dd1d4ad0e88bd4465f51b7581d885d
richardnd5 <nathanrichard@Nathans-MacBook-Pro.local>
2016-04-06 21:25:03 -0700

    Initial Commit After Meetup

---
+++ src/modules/comments/api.babel.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+let comments = [
+  {
+    name: 'Juan Shawn',
+    message: 'so what? APId'
+  }
+]
+
+app.get('/comments', (req, res) => {
+  res.send(comments)
+})
+
+app.post('/comments'), (req, res) => {
+
+  comments.push(req.body)
+  
+  res.send(comments);
+}
+
+app.listen(PORT, (err) => {
+  if(err) {
+    console.error(err)
+  }
+
+  console.info(`app listening on http://localhost:${PORT}`);
+})
