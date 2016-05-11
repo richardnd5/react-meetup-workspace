64da9103e7babba63d9886fe6728480b25481a16
Collin <candrew022@gmail.com>
2016-05-11 19:53:24 -0400

    Remove extra store

--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, browserHistory } from 'react-router';
-import {createStore, combineReducers} from 'redux';
 import {Provider} from 'react-redux';
 
 import routes from './routes';
-import * as reducers from './reducers';
-
-console.log(reducers)
-
-const reducer = combineReducers(reducers);
-const store = createStore(reducer);
 import store from './store';
 
 ReactDOM.render(
