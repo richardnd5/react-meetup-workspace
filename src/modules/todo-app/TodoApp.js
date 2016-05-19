d0bd10df4efee8d7a6b29d61ac0388bcd39c4163
Collin <candrew022@gmail.com>
2016-05-19 10:26:52 -0400

    Connect TodoApp to reducer state

---
+++ src/modules/todo-app/TodoApp.js
@@ -0,0 +1,22 @@
+import React, { Component }from 'react';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+import * as todoActions from './actions/todoActions';
+
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList'
+
+const TodoApp = ({state, actions}) => (
+  <div>
+    <h1>Todos</h1>
+    <TodoForm handleSubmit={actions.handleSubmit}/>
+    <TodoList todos={state.todos} onTodoClick={actions.handleTodoClick}/>
+  </div>
+)
+
+export default connect(state => ({
+  state: state.todo
+}),
+(dispatch) => ({
+  actions: bindActionCreators(todoActions, dispatch)
+}))(TodoApp);
