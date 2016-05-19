38a7923e8a5199966a6cb96d6b13567df8bad0a0
Collin <candrew022@gmail.com>
2016-05-19 10:21:27 -0400

    Separate Todo into own file

---
+++ src/modules/todo-app/components/Todo.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class Todo extends Component {
+
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.props.onClick(this.props.id);
+  }
+
+  render() {
+    return (
+      <div onClick={this.handleClick} style={
+        {textDecoration: this.props.completed ? 'line-through' : 'none'}}>
+        {this.props.text}
+      </div>
+    )
+  }
+}
+
+export default Todo;
